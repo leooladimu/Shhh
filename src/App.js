577a392926de9cfcc017c117c3d166e08b7a3509
Leo Oladimu <leooladimu9@gmail.com>
2022-03-17 17:53:01 -0400

    I think Alan screwed me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+const App = () => {
+  const [cart, setCart] = useState([])
+  const [menuItems, setMenuItems] = useState([])
+
+  useEffect(() => {
+    alanBtn({
+      key: '56d70623604d20c15d64abcb3a33ec2e2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData) => {
+        if (commandData.command === 'getMenu') {
+          setMenuItems(commandData.data)
+        } 
+      },
+    })
+  }, []);
+  
+  const addToCart = (menuItem) => {
+    setCart((oldCart) => {
+      return [...oldCart, menuItem]
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { menuItems.map(menuItem => (
+        <li key={ menuItem.name }>
+          { menuItem.name } - ${ menuItem.price } - { menuItem.category }
+          <button onClick={() => addToCart(menuItem)}>Add to Cart</button>
+        </li>
+      ))}
+      <h2>Cart</h2>
+      { cart.map(cartItem => (
+        <li key={ cartItem.name }>
+          { cartItem.name } - ${ cartItem.price } - { cartItem.category }
+        </li>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
